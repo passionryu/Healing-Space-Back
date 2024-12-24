509c1e61c5530446b128a2f5ac6ce2f525fb6417
passionryu <rsy1225@naver.com>
2024-12-25 00:59:04 +0900

    [chore] 멤버 도메인 주석 정리 및 가독성 증가를 위한 코드 정리

--- Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/Member/Service/MemberService.java
+++ Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/Member/Service/MemberService.java
@@ -5,20 +5,20 @@ import website.server.Domain.Member.DTO.Request.MemberRequest;
 public interface MemberService {
 
     /**
-     * 회원 가입
+     * 회원 가입 인터페이스
      * @param request 회원가입 정보 객체
      * @return 회원가입 후 생성된 ID
      */
     Long register(MemberRequest request);
 
     /**
-     * 아이디 찾기
-     * @return
+     * 아이디 찾기 인터페이스
+     * @return 아이디
      */
     Long findID();
 
     /**
-     * 비밀번호 변경
+     * 비밀번호 변경 인터페이스
      * @return
      */
     String changePW();
