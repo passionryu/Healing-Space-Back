4d6a29e1fbbb687597916aa4d4e3ad89a4319870
passionryu <rsy1225@naver.com>
2024-12-27 03:07:17 +0900

    [feat] 메인 페이지 화면 1차 개발

---
+++ Personal-HomePage-Project-main/MyProject/frontend/src/assets/js/main_slide.js
@@ -0,0 +1,14 @@
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+const slideTrack = document.querySelector('.slide-track');
+const totalSlides = slides.length;
+
+function moveSlide() {
+    index++;
+    if (index >= totalSlides) {
+        index = 0; // 마지막 슬라이드 후 첫 번째 슬라이드로 돌아감
+    }
+    slideTrack.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(moveSlide, 4500); // 4.5초마다 슬라이드 이동
