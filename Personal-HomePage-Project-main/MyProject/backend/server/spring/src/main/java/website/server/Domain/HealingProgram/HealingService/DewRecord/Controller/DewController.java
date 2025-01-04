d81bde492a3cc796ccc6627c333767f4044dfa95
passionryu <rsy1225@naver.com>
2025-01-05 00:01:00 +0900

    [fix] dewcontroller의 일부 api를 마이페이지 도메인으로 이동

--- Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/HealingProgram/HealingService/DewRecord/Controller/DewController.java
+++ Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/HealingProgram/HealingService/DewRecord/Controller/DewController.java
@@ -5,23 +5,21 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import website.server.Domain.HealingProgram.HealingService.DewRecord.DTO.Request.DiaryRequest;
 import website.server.Domain.HealingProgram.HealingService.DewRecord.DTO.Response.AiResponse;
-import website.server.Domain.HealingProgram.HealingService.DewRecord.DTO.Response.DiaryResponse;
-import website.server.Domain.HealingProgram.HealingService.DewRecord.DTO.Response.DiaryThumbnailResponse;
 import website.server.Domain.HealingProgram.HealingService.DewRecord.Service.DewService;
 import website.server.Global.JWT.JwtService;
-import java.time.LocalDate;
-import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dew")
-@Tag(name = "DEW Calendar", description = "Dew 캘린더 서비스 API")
+@Tag(name = "DEW Record", description = "Dew 레코드 서비스 API")
 public class DewController {
 
     /*
@@ -49,58 +47,6 @@ public class DewController {
         dewService.saveDiary(jwtService.extractUserNumberFromRequest(request),diaryRequest,aiResponse);
 
         return ResponseEntity.ok(aiResponse);
-    }
-
-    /**
-     * 일기 삭제 API
-     * @param request 사용자 요청
-     * @param date 일기 작성 날짜
-     * @return 일기 삭제 성공 메시지
-     */
-    @Operation(summary = " 일기 삭제 API ", description = " ")
-    @DeleteMapping("/diary")
-    public ResponseEntity<String> deleteDiary(HttpServletRequest request,
-                                              @RequestParam(name = "date")
-                                              @DateTimeFormat(pattern = "yyyy-MM-dd")
-                                              LocalDate date){
-
-        /* 일기 삭제 */
-        dewService.deleteDiary(request,date);
-
-        return ResponseEntity.ok("일기 삭제 성공!");
-    }
-
-    /**
-     * 일기 리스트 조회 API
-     * @param request 사용자 요청
-     * @return 내 일기 리스트(diaryNumber,weather,date,title)
-     */
-    @Operation(summary = " 일기 리스트 조회 API ", description = " ")
-    @PostMapping("/diary/show/list")
-    public ResponseEntity<List<DiaryThumbnailResponse>> showDiaryList(HttpServletRequest request){
-
-        /* 다이어리 DTO 리스트 반환 */
-        List<DiaryThumbnailResponse> diaryThumbnailResponse = dewService.getDiaryThumbnails(request);
-
-        return ResponseEntity.ok(diaryThumbnailResponse);
-    }
-
-    /**
-     * 일기장 조회 API
-     * @param request 사용자 요청
-     * @return 일기장 (날짜,제목,본문,감정,힐링 메시지,힐링 뮤직)
-     */
-    @Operation(summary = " 일기장 조회 API ", description = " ")
-    @PostMapping("/diary/show")
-    public ResponseEntity<DiaryResponse> showDiary(HttpServletRequest request,
-                                                   @RequestParam(name = "date")
-                                                   @DateTimeFormat(pattern = "yyyy-MM-dd")
-                                                   LocalDate date){
-
-        /* 선택한 날짜의 일기 데이터 반환 */
-        DiaryResponse diaryResponse = dewService.getDiary(request,date);
-
-        return ResponseEntity.ok(diaryResponse);
     }
 
 }
