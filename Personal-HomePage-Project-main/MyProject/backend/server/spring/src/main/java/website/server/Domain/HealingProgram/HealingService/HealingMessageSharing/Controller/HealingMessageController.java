e1438963af2ff86d885e3c0f0e9d0e32f4ab914a
passionryu <rsy1225@naver.com>
2025-01-14 16:13:31 +0900

    [refactor] 주석 정리 및 SQL 리펙토링

--- Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/HealingProgram/HealingService/HealingMessageSharing/Controller/HealingMessageController.java
+++ Personal-HomePage-Project-main/MyProject/backend/server/spring/src/main/java/website/server/Domain/HealingProgram/HealingService/HealingMessageSharing/Controller/HealingMessageController.java
@@ -141,7 +141,7 @@ public class HealingMessageController {
         return ResponseEntity.ok(healingMessageService.getMyLikeMessageList(request));
     }
 
-    // todo Connecting
+    // connected
     /**
      * 좋아요 누른 힐링 메시지 상세 조회 API
      * @param request 사용자 요청
@@ -158,6 +158,7 @@ public class HealingMessageController {
     }
 
     /* 힐링 메시지에 댓글 달기 */
+
 
     /* 힐링 메시지 수정하기 - 추후 ver 2 업그레이드 */
 
